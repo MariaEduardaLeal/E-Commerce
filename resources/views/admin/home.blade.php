ded9f773e2dc365cf18fb910da3602baeba4625b
MariaEduardaLeal <eduardaleal753@gmail.com>
2024-10-23 19:29:24 -0300

    Faz algumas mudanças no fornt

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin</title>
-</head>
-<body>
-    <h1>TESTEEEEEEEEEEEEEEEE</h1>
-</body>
-</html>
+<x-app-layout>
+
+</x-app-layout>
+
+
