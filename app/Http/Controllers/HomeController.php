567fd78a9049068a4ce2f01650a06350a22cdb57
MariaEduardaLeal <eduardaleal753@gmail.com>
2024-10-23 19:11:17 -0300

    Cria rota personalizada para view dependendo do tipo de usuário

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    public function redirect()
+    {
+        $user_type = Auth::user()->usertype;
+
+        if($user_type =='1'){
+            return view('admin.home');
+        }else{
+            return view('dashboard');
+        }
+    }
+}
