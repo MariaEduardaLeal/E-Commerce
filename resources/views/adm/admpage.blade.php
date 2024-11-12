f9f8c70ae2906e2eb21bcd8774748b7215a87df4
MariaEduardaLeal <eduardaleal753@gmail.com>
2024-11-12 15:45:58 -0300

    Adiciona novas funções

--- resources/views/adm/admpage.blade.php
+++ resources/views/adm/admpage.blade.php
@@ -18,10 +18,6 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="{{ asset('js/accessibility_menu.js') }}" defer></script>
-    <script src="{{ asset('js/admpage.js') }}"></script>
-
-
-
 </head>
 
 <body>
@@ -38,7 +34,6 @@
             <label for="productPrice">Preço</label>
             <input type="text" id="productPrice" name="price" placeholder="Digite o preço" required />
 
-
             <label for="productDescription">Descrição</label>
             <textarea id="productDescription" name="description" placeholder="Descrição do produto" rows="3"></textarea>
 
@@ -51,31 +46,81 @@
             </select>
 
             <!-- Campo de cores -->
-            <label>Cores</label>
-            <div id="colorInputs" class="flex items-center">
-                <input type="color" name="color[]" placeholder="Cor (ex: Vermelho)" />
-                <button type="button" id="addColorBtn" class="add-button">Adicionar Cor</button>
+            <div id="colorContainer" style="display: none;">
+                <label>Cores</label>
+                <div id="colorInputs" class="flex items-center">
+                    <input id="color_input" type="color" name="color[]" value="#ffffff" />
+                    <button type="button" id="addColorBtn" class="add-button">Adicionar Cor</button>
+                </div>
             </div>
 
+
             <!-- Campo de tamanhos -->
-            <label>Tamanhos</label>
-            <div id="sizeButtons" class="flex flex-wrap">
-                <button type="button" class="size-button">PP</button>
-                <button type="button" class="size-button">P</button>
-                <button type="button" class="size-button">M</button>
-                <button type="button" class="size-button">G</button>
-                <button type="button" class="size-button">GG</button>
-                <button type="button" class="size-button">XG</button>
-                <button type="button" class="size-button">XGG</button>
+            <div id="sizeContainer" style="display: none;">
+                <label>Tamanhos</label>
+                <div id="sizeButtons" class="flex flex-wrap">
+                    <button type="button" class="size-button">PP</button>
+                    <button type="button" class="size-button">P</button>
+                    <button type="button" class="size-button">M</button>
+                    <button type="button" class="size-button">G</button>
+                    <button type="button" class="size-button">GG</button>
+                    <button type="button" class="size-button">XG</button>
+                    <button type="button" class="size-button">XGG</button>
+                </div>
             </div>
 
             <label for="productImages">Imagens</label>
-            <input type="file" id="productImages" name="images[]" accept="image/png, image/jpeg" multiple />
+            <input type="file" id="productImages" name="images[]" accept="image/png, image/jpeg" multiple required />
 
             <button type="submit" class="submit-btn">Cadastrar Produto</button>
         </form>
     </div>
     @include('includes.footer')
+
+    <script>
+        $(document).ready(function() {
+
+            $('#productPrice').mask('R$ 000.000.000,00', {
+                reverse: true
+            });
+
+            // Exibe ou oculta os campos de acordo com o tipo de produto selecionado
+            $('#productType').on('change', function() {
+                var selectedType = $(this).val();
+
+
+                if (selectedType === 'roupas' || selectedType === 'eletrodomesticos') {
+                    $('#colorContainer').show();
+                } else {
+                    $('#colorContainer').hide();
+                }
+
+
+                if (selectedType === 'roupas') {
+                    $('#sizeContainer').show();
+                } else {
+                    $('#sizeContainer').hide();
+                }
+            });
+
+
+            $('#addColorBtn').on('click', function() {
+
+                let currentColor = $('#color_input').val();
+
+                $('#colorInputs').append('<input disabled type="color" name="color[]" value="' + currentColor + '" />');
+            });
+
+
+
+            $('#productForm').on('submit', function(e) {
+                if ($('#productImages')[0].files.length === 0) {
+                    e.preventDefault();
+                    alert('Por favor, adicione pelo menos uma imagem.');
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
